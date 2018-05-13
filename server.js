cf07080c42cabf170a6ee1f0005e8f20ba39c65e
Quoc Man <quocman1982@gmail.com>
2018-05-13 16:02:53 +0700

    .

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine','hbs');
+
+app.use((req,res,next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    fs.appendFile('server.log',log + '\n',(err)=>{
+        if(err){
+            console.log('Unable to append to server.log');
+        }
+    });
+    //console.log(`${now}: ${req.method} ${req.url}`);
+    next();
+});
+// app.use((req,res,next) => {
+    
+//     res.render('maintenance.hbs');
+// });
+app.use(express.static(__dirname + '/public'))
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear();
+})
+hbs.registerHelper('screamIt',(text)=>{
+    return text.toUpperCase();
+})
+app.get('/',(req, res) =>{
+    //res.send('<h1>Hello Express!</h1>');
+    // res.send({
+    //     name:'Andrew',
+    //     likes:[
+    //         'football','game'
+    //     ]
+    // })
+    res.render('home.hbs',{
+        pageTitle:'Home page'
+    });
+})
+
+app.get('/about',(req, res) =>{
+    //res.send('<h1>About Me!</h1>');
+    res.render('about.hbs',{
+        pageTitle:'About page2'
+    });
+})
+app.listen(3000, () => {
+    console.log('Server is up on 3000')
+});
